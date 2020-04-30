6997b5b2c9547b0092b4474b0b368f644dc162df
edaral3 <arnolso201@gmail.com>
2020-04-29 20:07:44 -0600

    nuevo proyecto

--- ProjectAPP/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class usuario(models.Model):
-    nombre = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-
